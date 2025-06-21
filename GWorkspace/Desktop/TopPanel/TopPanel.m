5c3358242ab7ed2a24004e3101980f50af596e54
pkgdemon <pkgdemonteam@gmail.com>
2025-06-21 18:24:05 -0500

    -Update panel height

--- GWorkspace/Desktop/TopPanel/TopPanel.m
+++ GWorkspace/Desktop/TopPanel/TopPanel.m
@@ -4,7 +4,7 @@
 @implementation TopPanel
 
 - (instancetype)initForManager:(GWDesktopManager *)manager {
-    self = [super initWithFrame:NSMakeRect(0, 0, 100, 30)];
+    self = [super initWithFrame:NSMakeRect(0, 0, 100, 28)];
     if (self) {
         _manager = manager;
         [self setAutoresizingMask:NSViewWidthSizable];
@@ -20,7 +20,7 @@
 
 - (void)tile {
     NSRect screen = [[NSScreen mainScreen] frame];
-    CGFloat height = 30.0;
+    CGFloat height = 28.0;
     [self setFrame:NSMakeRect(0, screen.size.height - height, screen.size.width, height)];
     [self setNeedsDisplay:YES];
 }
